036a159fb0aa9449cf5341deaf6d35d3bbc373bd
Abderrazzak Hmidat <abhmidat@e1r8p10.1337.ma>
2025-12-09 15:04:43 +0100

    just fix long function

--- srcs/draw_map.c
+++ srcs/draw_map.c
@@ -1,4 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   draw_map.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abhmidat <abhmidat@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/09 13:02:34 by abhmidat          #+#    #+#             */
+/*   Updated: 2025/12/09 14:20:56 by abhmidat         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../include/cub_3d.h"
+
+static int calculate_and_clamp_texture_x(t_ray *ray, t_image *texture)
+{
+    double wall_x;
+    int    tex_x;
+
+    if (ray->is_vertical_hit == 1)
+        wall_x = ray->ray_y / TILE_SIZE;
+    else
+        wall_x = ray->ray_x / TILE_SIZE;
+    
+    wall_x = wall_x - floor(wall_x);
+    tex_x = (int)(wall_x * texture->width);
+    
+    if (tex_x < 0)
+        tex_x = 0;
+    if (tex_x >= texture->width)
+        tex_x = texture->width - 1;
+    
+    return (tex_x);
+}
+
+static int init_wall_draw_params(t_data *data, int ray_index, 
+                                   double wall_top, double wall_bottom)
+{
+    t_image *texture;
+    int     tex_x;
+    
+    texture = texture_decide(data, &data->rays[ray_index]);
+    data->wall.wall_height = (int)(wall_bottom - wall_top);
+    data->wall.wall_start = (int)wall_top;
+    data->wall.wall_end = (int)wall_bottom;
+    
+    if (data->wall.wall_start < 0)
+        data->wall.wall_start = 0;
+    if (data->wall.wall_end >= WIN_HEIGHT)
+        data->wall.wall_end = WIN_HEIGHT;
+    
+    tex_x = calculate_and_clamp_texture_x(&data->rays[ray_index], texture);
+    
+    return (tex_x);
+}
 
 void draw_wall(t_data *data, int ray_index, double wall_top, double wall_bottom)
 {
@@ -7,42 +61,15 @@ void draw_wall(t_data *data, int ray_index, double wall_top, double wall_bottom)
     int         tex_y;
     int         pixel_color;
     t_image     *texture;
-    t_ray       *ray;
-    double      wall_x;
-    int         line_height;
-    int         draw_start;
-    int         draw_end;
 
-    ray = &data->rays[ray_index];
-    texture = texture_decide(data, ray);
-    
-    line_height = (int)(wall_bottom - wall_top);
-    draw_start = (int)wall_top;
-    draw_end = (int)wall_bottom;
-    
-    if (ray->is_vertical_hit == 1)    // FIX: Divide by TILE_SIZE to get fractional position
-        wall_x = ray->ray_y / TILE_SIZE;  // ← ADD THIS
-    else
-        wall_x = ray->ray_x / TILE_SIZE;  // ← ADD THIS
-    
-    wall_x = wall_x - floor(wall_x);    // Get fractional part (0.0 to 1.0)
-    
-    tex_x = (int)(wall_x * texture->width);    // Calculate texture X
-    if (tex_x < 0)
-        tex_x = 0;
-    if (tex_x >= texture->width)
-        tex_x = texture->width - 1;
-    
-    if (draw_start < 0)    // Clamp to screen
-        draw_start = 0;
-    if (draw_end >= WIN_HEIGHT)
-        draw_end = WIN_HEIGHT;
+    tex_x = init_wall_draw_params(data, ray_index, wall_top, wall_bottom);
+    texture = texture_decide(data, &data->rays[ray_index]);
 
-    y = draw_start;     // Draw pixels
-    while (y < draw_end)
+    y = data->wall.wall_start;
+    while (y < data->wall.wall_end)
     {
-        int d = y * 256 - WIN_HEIGHT * 128 + line_height * 128;
-        tex_y = ((d * texture->height) / line_height) / 256;
+        int d = y * 256 - WIN_HEIGHT * 128 + data->wall.wall_height * 128;
+        tex_y = ((d * texture->height) / data->wall.wall_height) / 256;
         
         if (tex_y < 0)
             tex_y = 0;
@@ -51,7 +78,6 @@ void draw_wall(t_data *data, int ray_index, double wall_top, double wall_bottom)
         
         pixel_color = texture_get_pixel(texture, tex_x, tex_y);
         put_pixel_to_buffer(&data->buffer, ray_index, y, pixel_color);
-        
         y++;
     }
 }
